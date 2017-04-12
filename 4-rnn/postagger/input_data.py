4099375eaed4b2c84f57dc521930f759fe5b8904
hpzhao <huaipengzhao@gmail.com>
2017-04-12 21:49:38 +0800

    fix some bugs

--- 4-rnn/postagger/input_data.py
+++ 4-rnn/postagger/input_data.py
@@ -2,36 +2,30 @@
 #coding:utf8
 import sys
 import numpy
+import os
 import cPickle as pkl
-sys.path.append('../../1-utils/')
+sys.path.append('../../1-utils')
 from dataset import Dataset
-'''
-embedding[0] = padding
-embedding[1] = UNK
-'''
-
-def read_dataset(filename):
-    label_hash = pkl.load(open('../../data/PTB/label2id.pkl'))
-    word_hash = pkl.load(open('../../data/PTB/word2id.pkl'))
+def read_dataset(data):
+    if os.path.isfile(data+'.pkl'):
+        return pkl.load(open(data+'.pkl'))
+    label2id = pkl.load(open('../../data/PTB/label2id.pkl'))
+    word2id = pkl.load(open('../../data/PTB/word2id.pkl'))
     sents_list = []
     labels_list = []
-    for line in open(filename):
+    for line in open(data):
         tokens = line.strip().split()
         word_list = []
         label_list = []
         for i in range(len(tokens)):
             word = tokens[i].rsplit('/',1)[0]
-            word_list.append(word_hash[word] if word in word_hash else 1)
+            word_list.append(word2id[word] if word in word2id.keys() else 1)
             label = tokens[i].rsplit('/',1)[1]
-            label_list.append(label_hash[label] if label in label_hash else 0)
+            assert label in label2id.keys(),'unknow label'
+            label_list.append(label2id[label])
+        
         sents_list.append(word_list)
         labels_list.append(label_list)
-    sents = numpy.array(sents_list)
-    labels = numpy.array(labels_list)
-    dataset = Dataset(sents,labels)
+    dataset = Dataset(sents_list,labels_list)
+    pkl.dump(dataset,open(data + '.pkl','w'))
     return dataset
-
-if __name__=='__main__':
-    labels = pkl.load(open('../../data/PTB/label2id.pkl'))
-    for key in labels.keys():
-        print key
